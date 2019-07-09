ddf798d7e6f0e73d65ac765e5dd3b8ac89fde2c3
Isaiah Pius <isaiahtech1@gmail.com>
2019-07-09 12:09:45 +0100

    Database Migrations

---
+++ database/migrations/2019_07_04_163834_create_forums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forums', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('articeable_type')->unsigned();
+            $table->string('articeable_id');
+            $table->enum('approval', ["accepted", "denied"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forums');
+    }
+}
